0a3276a4cddd905d8c05a596a54c50e6ec58b723
Oluwatobi <amure387@gmail.com>
2022-09-08 14:52:27 +0100

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	char a;
-	int  b;
+	int b;
 	long c;
 	long long d;
 	float e;
-printf("Size of a char: %lu byte(s)\n", sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
-       	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+printf("Size of a char: %li byte(s)", sizeof(a));
+printf("Size of an int: %li byte(s)", sizeof(b));
+printf("Size of a long int: %li byte(s)", sizeof(c));
+printf("Size of a long long int: %li byte(s)", sizeof(d));
+printf("Size of a float: %li byte(s)", sizeof(e));
+return (0);
 
-	return (0);
 }

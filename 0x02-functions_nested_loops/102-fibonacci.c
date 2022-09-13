547774bda78244c3d9b9b97175d2f5ca2ffb9240
Oluwatobi <amure387@gmail.com>
2022-09-13 16:13:19 +0100

    another commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int f1 = 1;
+	int f2 = 2, i;
+	printf("%i, %i, ", f1, f2);
+	for (i = 3; i < 51; i++)
+	{
+		long next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+		printf("%lu", next);
+		if (i != 50)
+			printf(", ");
+	}
+	printf("\n");
+}

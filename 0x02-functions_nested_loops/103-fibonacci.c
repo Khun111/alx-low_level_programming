bd2162bdc7c851e892c581f3b3f594300fac8fe7
Oluwatobi <amure387@gmail.com>
2022-09-13 16:37:37 +0100

    another commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int f1 = 1;
+	int f2 = 2, i;
+	long track = 0;
+
+	while (f2 < 4000000)
+	{
+
+		long next = f1 + f2;
+
+		f1 = f2;
+		f2 = next;
+		if (next % 2 == 0)
+			track += next;
+	}
+	printf("%lu\n", track);
+}

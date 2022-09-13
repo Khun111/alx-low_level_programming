7a10fcf722ba8405979efd1a260d107296ce2dbb
Oluwatobi <amure387@gmail.com>
2022-09-13 15:29:57 +0100

    a commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,10 +15,59 @@ void print_times_table(int n)
 {
 	if (!(n > 15 || n < 0))
 	{
+		int i = 0;
 
+		while (i <= n)
+		{
+			int j = 0;
+
+			while (j <= n)
+			{
+				int multi = i * j;
+				if (multi < 10)
+				{
+					if (j == 0)
+					{
+						print_int(multi);
+						_putchar(',');
+						_putchar(' ');
+					}
+					_putchar(' ');
+					_putchar(' ');
+					print_int(multi);
+					if (j != n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else if (multi < 100)
+				{
+					_putchar(' ');
+					print_int(multi);
+					if (j != n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					print_int(multi);
+					if (j != n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
 	}
 }
 int main(void)
 {
-	print_int(110);
+	print_times_table(17);
 }

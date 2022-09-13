28089f38691d90093b92ed07d2015d5d8dbb8fb9
Oluwatobi <amure387@gmail.com>
2022-09-13 16:15:47 +0100

    another commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,10 +7,12 @@ int main(void)
 {
 	int f1 = 1;
 	int f2 = 2, i;
+
 	printf("%i, %i, ", f1, f2);
 	for (i = 3; i < 51; i++)
 	{
 		long next = f1 + f2;
+
 		f1 = f2;
 		f2 = next;
 		printf("%lu", next);

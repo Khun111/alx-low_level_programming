2ada5b69adcea9aa4e4e650e8328bd4e8623c224
Oluwatobi <amure387@gmail.com>
2022-09-21 16:20:50 +0100

    empty

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * infinite_add - complicated
+ * @n1: first
+ * @n2: second
+ * @r: third
+ * @size_r: fourth
+ * Return: string
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_buffer - complicated
+ * @b: first
+ * @size: second
+ */
+void print_buffer(char *b, int size)
+{
+
+}

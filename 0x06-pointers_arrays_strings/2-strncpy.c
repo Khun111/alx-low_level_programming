7f6993d9bfd5142dd23b3ffa9cb909f80c772ed0
Oluwatobi <amure387@gmail.com>
2022-09-21 20:38:29 +0100

    checker

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (; i < n; i++)
+	for (; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }

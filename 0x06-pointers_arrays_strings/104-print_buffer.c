9f14f30f98edb1dd1b9a05143b68274c6029b759
Oluwatobi <amure387@gmail.com>
2022-09-23 01:13:11 +0100

    finally

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,6 +7,37 @@
  */
 void print_buffer(char *b, int size)
 {
-	char c[100];
-	sprintf(c, b, size);
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		int i = 0, j, k;
+
+		for (; i < size; i += 10)
+		{
+			printf("%08x", i);
+				j = i;
+			while (j < i + 10)
+			{
+				if (j % 2 == 0)
+					printf(" ");
+				if (j < size)
+					printf("%.2x", b[j]);
+				else
+					printf(" ");
+				j++;
+			}
+			printf(" ");
+			k = i;
+			while (k < i + 10)
+			{
+				if ((b[k] > 126) || (b[k] < 32))
+					printf(".");
+				else
+					printf("%c", b[k]);
+				k++;
+			}
+		printf("\n");
+		}
+	}
 }

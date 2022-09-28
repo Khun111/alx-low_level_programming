dc4f5b031525b48c9f2f6f471e851c02f3555baf
Oluwatobi <amure387@gmail.com>
2022-09-28 18:07:37 +0100

    checker

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * wildcmp - wildcard
+ * @s1: param
+ * @s2: param
+ * Return: num
+ */
 int wildcmp(char *s1, char *s2)
 {
 	if ((*s1 == '\0') && (*s2 == '\0'))
@@ -9,11 +15,11 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s1 != '\0')
 		{
-			if (*(s2 + 1) == *(s1 + 1))
-				return (wildcmp(s1 + 1, s2 + 1));
+			if (*(s2 + 1) == '*' || *(s2 + 1) == *(s1 + 1))
+				return (wildcmp(s1, s2 + 1));
 		}
 		if (*s1 == '\0')
-			return (0);
+			return (wildcmp(s1 + 1, s2 + 1));
 		return (1);
 	}
 	return (0);

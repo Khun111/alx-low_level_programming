0e28a999c8e0a14aaa821f48412fa688f217d941
Oluwatobi <amure387@gmail.com>
2022-09-28 11:00:22 +0100

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,6 +7,8 @@
  */
 int isPrime(int n, int i)
 {
+	if (n == 1)
+		return (0);
 	if (i != 1)
 	{
 		if (n % i != 0)
@@ -16,7 +18,7 @@ int isPrime(int n, int i)
 	return (1);
 }
 /**
- * isPrime - check for prime
+ * is_prime_number - check for prime
  * @n: param
  * Return: 0/1
  */

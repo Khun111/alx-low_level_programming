fb067d4fc716cc56006c807911659274201ed0d7
Oluwatobi <amure387@gmail.com>
2022-09-28 09:23:40 +0100

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,14 @@
-
+#include "main.h"
+int _sqrt(int n, int b)
+{
+	if (b * b == n && ((b + 1) * (b + 1) > n))
+		return (b);
+	else if (b * b < n && ((b + 1) * (b + 1) > n))
+		return (-1);
+	else
+		return (_sqrt(n, b + 1));
+}
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}

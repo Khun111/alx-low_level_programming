ab15df24f86776e88f2729411f864a918a1df56b
Oluwatobi <amure387@gmail.com>
2022-10-10 08:01:40 +0100

    push

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+Moving into structures. Will there also be unions?
+Personally, I've felt structures were very similar to objects in most high level languages.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - structure representing a dog
+ * @name: dog name
+ * @age: age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner
+}
+#endif

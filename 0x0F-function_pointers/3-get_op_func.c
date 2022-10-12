9ed48c23f1f02615a240d7c4588f3e55d9d6a472
Oluwatobi <amure387@gmail.com>
2022-10-12 11:49:33 +0100

    push

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,8 +13,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*s == ops[i].op)
-			return (ops[i].f)
+		if (*s == *(ops[i].op))
+			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+int main(int count, char **value)
+{
+	char *arith;
+	int num1, num2, result;
+
+	if (count != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num2 = atoi(value[3]);
+	num1 = atoi(value[1]);
+	arith = value[2];
+	if (get_op_func(arith) == NULL || arith[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (*arith == '/' || *arith == '%')
+	{
+		if (num2 == 0)
+		{
+			printf("Error\n");
+			exit(100);
+		}
+	}
+	result = get_op_func(arith)(num1, num2);
+	printf("%i\n", result);
+	return (0);
+}

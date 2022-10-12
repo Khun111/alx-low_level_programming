b04711c025cab86dd64dd4384b488b3895e9de22
Oluwatobi <amure387@gmail.com>
2022-10-12 15:03:22 +0100

    final

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print opcodes of main
+ * @count: args number
+ * @value: args
+ * Return: 0 on success
+ */
+int main(int count, char **value)
+{
+	char *ref = (char *)main;
+	int i = 0, num;
+
+	if (count != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num = atoi(value[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for(; i < (num - 1); i++)
+		printf("%.02x ", ref[i]);
+	printf("%.02x", ref[i]);
+	printf("\n");
+	return (0);
+}

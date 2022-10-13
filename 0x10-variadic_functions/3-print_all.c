d6b3e869d79a915de6d2f08e6c38a5d541270a4a
Oluwatobi <amure387@gmail.com>
2022-10-13 15:22:55 +0100

    push

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+void print_all(const char * const format, ...)
+{
+	int j = 0, l = strlen(format) - 1;
+	char *x;
+	va_list args;
+
+	va_start(args, format);
+	while(format && (j <= l))
+	{
+		switch (format[j])
+		{
+			case 'c': printf("%c", va_arg(args, int));
+					  separator(j, l);
+					  break;
+			case 'i': printf("%i", va_arg(args, int));
+					  separator(j, l);
+					  break;
+			case 'f': printf("%f", va_arg(args, double));
+					  separator(j, l);
+					  break;
+			case 's': x = va_arg(args, char *);
+					  if(!x)
+						  x = "(nil)";
+					  printf("%s", x);
+					  break;
+		}
+		j++;
+	}
+	putchar('\n');
+}
+void separator(int a, int b)
+{
+	if (a != b)
+		printf(", ");
+}

4e1ffbdf7e9ab1bdb458aebcb1090f0ad7420d0a
Oluwatobi <amure387@gmail.com>
2022-10-24 22:21:35 +0100

    push

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,15 +7,15 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *trav;
+	/*listint_t *trav;*/
 	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
-	trav = head;
-	while (i++ < index)
-		trav = trav->next;
-	if (trav)
-		return (trav);
+	/*trav = head;*/
+	while (i++ < index && head)
+		head = head->next;
+	if (head)
+		return (head);
 	return (NULL);
 }

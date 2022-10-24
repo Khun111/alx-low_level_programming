e4e9f42f18fee4938a26d503ce78163e9f526616
Oluwatobi <amure387@gmail.com>
2022-10-24 20:58:21 +0100

    push

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,15 +8,19 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *trav, *prev;
-	unsigned int i = 0;
+	unsigned int i = 0, l;
 
-	if (*head == NULL || !head)
+	if (!head || *head == NULL)
+		return (-1);
+	l = listint_len(*head);
+	if (l == 0 || l <= index)
 		return (-1);
 	trav = *head;
 	if (index == 0)
 	{
 		*head = trav->next;
 		free(trav);
+		return (1);
 	}
 	else
 	{
@@ -27,6 +31,24 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		prev->next = trav->next;
 		free(trav);
+		return (1);
 	}
-	return (1);
+	return (-1);
+}
+/**
+ * listint_len - list items in list
+ * @h: list head
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *trav = h;
+	int count = 0;
+
+	while (trav != NULL)
+	{
+		count++;
+		trav = trav->next;
+	}
+	return (count);
 }

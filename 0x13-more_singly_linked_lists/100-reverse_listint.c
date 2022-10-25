04eb45c9e245e77f9154d2f5b0ca3d1b2618a1fd
Oluwatobi <amure387@gmail.com>
2022-10-25 10:05:51 +0100

    push

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * reverse_listint - function to reverse list
+ * @head: pointer to head node
+ * Return: reversed head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
+}

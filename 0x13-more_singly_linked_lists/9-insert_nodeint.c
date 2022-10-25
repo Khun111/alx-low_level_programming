7b481fddc3712f8a3871833c596bad5e6e7407fc
Oluwatobi <amure387@gmail.com>
2022-10-25 21:28:20 +0100

    push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,12 +9,21 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *trav = *head, *addidx = malloc(sizeof(listint_t));
-	unsigned int i = 1;
+	unsigned int i = 1, count = 0;
 
 	if (addidx == NULL)
 		return (NULL);
 	addidx->n = n;
 	addidx->next = NULL;
+	while (trav != NULL)
+	{
+		count++;
+		trav = trav->next;
+	}
+	if (idx > count)
+		return (NULL);
+	trav = *head;
+	
 	if (idx == 0)
 	{
 		addidx->next = *head;

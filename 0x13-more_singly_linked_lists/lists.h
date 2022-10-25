f79f5833f67106c8098fb717a864a68c091f7645
Oluwatobi <amure387@gmail.com>
2022-10-25 16:57:22 +0100

    push

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 size_t listint_len(const listint_t *h);
+listint_t *find_listint_loop(listint_t *head);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *reverse_listint(listint_t **head);

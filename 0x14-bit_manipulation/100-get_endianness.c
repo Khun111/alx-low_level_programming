e930370fb6c4045013cc522a4d84100ef6d28eb6
Oluwatobi <amure387@gmail.com>
2022-10-27 11:46:27 +0100

    push

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - detect the endianness
+ * architecture of your computer
+ * Return: 1/0
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	char *s = (char *)&n;
+
+	if(*s)
+		return (1);
+	return (0);
+}

4b203eb3ed31a79235438b73a4954c62782da343
Oluwatobi <amure387@gmail.com>
2022-10-27 13:03:38 +0100

    push

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,7 +7,7 @@
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int unmatch = n ^ m, set = 0;
+	unsigned long int unmatch = n ^ m, set = 0;
 
 	while (unmatch)
 	{

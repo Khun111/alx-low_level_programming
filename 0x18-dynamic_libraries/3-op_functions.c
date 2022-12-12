6d751318ef7ed0ef48b380465e43a26d1fa35d97
Oluwatobi <amure387@gmail.com>
2022-12-12 08:18:29 +0100

    push

---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - use relevant operator
+ * @a: param
+ * @b: param
+ * Return: result;
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - use relevant operator
+ * @a: param
+ * @b: param
+ * Return: result;
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - use relevant operator
+ * @a: param
+ * @b: param
+ * Return: result;
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - use relevant operator
+ * @a: param
+ * @b: param
+ * Return: result;
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - use relevant operator
+ * @a: param
+ * @b: param
+ * Return: result;
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

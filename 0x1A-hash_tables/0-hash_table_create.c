3032a09ba061b5881cec9b81af0f73bb3999e494
Oluwatobi <amure387@gmail.com>
2023-01-05 23:28:11 +0100

    push

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,14 @@
 #include "hash_tables.h"
+/**
+ * hash_table_create - function to create hash table
+ * @size: size of table
+ * Return: pointer to table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
+
 	if (!table)
 		return (NULL);
 	table->size = size;
